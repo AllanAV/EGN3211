01adcdde08625e181721a1d3200d4abd676fd51e
Allan V Aquino Vieira <aquinovieira@gmail.com>
2018-11-04 18:19:28 -0500

    Add files via upload

--- EGN3211_HW6A.c
+++ EGN3211_HW6A.c
@@ -11,7 +11,7 @@ https://github.com/AllanAV/EGN3211/blob/master/EGN3211_HW6A.c
 #include <stdlib.h>
 
 //Union Definition
-union data
+union integer
 	{
 		char c;
 		short s;
@@ -22,98 +22,97 @@ union data
 int main (void)
 {
 	//Main Local Variable
-	union data userEntry;
-	userEntry.c = '8';
+	union integer integer;
 	
-	while(userEntry.c != '0')
+	while(integer.c != '0')
 	{
 	/*CHAR SECTION*/
 	printf("Enter data for type char (enter 0 to exit):");
-	scanf("%c", &userEntry.c);
+	scanf("%c", &integer.c);
 	fflush(stdin);
 	
-	if(userEntry.c == '0')
+	if(integer.c == '0')
 	{
 		break;
 	}
 	
 	printf("\nBreakdown of the element in the union\n");
-	printf("char %c\n", userEntry.c);
-	printf("short %hd\n", userEntry.s);
-	printf("int %d\n", userEntry.i);
-	printf("long %ld\n\n", userEntry.b);
+	printf("char %c\n", integer.c);
+	printf("short %hd\n", integer.s);
+	printf("int %d\n", integer.i);
+	printf("long %ld\n\n", integer.b);
 	
 	printf("Breakdown in hex\n");
-	printf("char %x\n", userEntry.c);
-	printf("short %x\n", userEntry.s);
-	printf("int %x\n", userEntry.i);
-	printf("long %x\n\n", userEntry.b);
+	printf("char %x\n", integer.c);
+	printf("short %x\n", integer.s);
+	printf("int %x\n", integer.i);
+	printf("long %x\n\n", integer.b);
 	
 	/*SHORT SECTION*/
 	printf("Enter data for type short (enter 0 to exit):");
-	scanf("%hd",&userEntry.s);
+	scanf("%hd",&integer.s);
 	fflush(stdin);
 	
-	if(userEntry.s == (short)0)
+	if(integer.s == (short)0)
 	{
 		break;
 	}
 	
 	printf("\nBreakdown of the element in the union\n");
-	printf("char %c\n", userEntry.c);
-	printf("short %hd\n", userEntry.s);
-	printf("int %d\n", userEntry.i);
-	printf("long %ld\n\n", userEntry.b);
+	printf("char %c\n", integer.c);
+	printf("short %hd\n", integer.s);
+	printf("int %d\n", integer.i);
+	printf("long %ld\n\n", integer.b);
 	
 	printf("Breakdown in hex");
-	printf("char %x\n", userEntry.c);
-	printf("short %x\n", userEntry.s);
-	printf("int %x\n", userEntry.i);
-	printf("long %x\n\n", userEntry.b);
+	printf("char %x\n", integer.c);
+	printf("short %x\n", integer.s);
+	printf("int %x\n", integer.i);
+	printf("long %x\n\n", integer.b);
 	
 	/*INT SECTION*/
 	printf("Enter data for type int (enter 0 to exit):");
-	scanf("%d",&userEntry.i);
+	scanf("%d",&integer.i);
 	fflush(stdin);
 	
-	if(userEntry.s == (int)0)
+	if(integer.s == (int)0)
 	{
 		break;
 	}
 	
 	printf("\nBreakdown of the element in the union\n");
-	printf("char %c\n", userEntry.c);
-	printf("short %hd\n", userEntry.s);
-	printf("int %d\n", userEntry.i);
-	printf("long %ld\n\n", userEntry.b);
+	printf("char %c\n", integer.c);
+	printf("short %hd\n", integer.s);
+	printf("int %d\n", integer.i);
+	printf("long %ld\n\n", integer.b);
 	
 	printf("Breakdown in hex");
-	printf("char %x\n", userEntry.c);
-	printf("short %x\n", userEntry.s);
-	printf("int %x\n", userEntry.i);
-	printf("long %x\n\n", userEntry.b);
+	printf("char %x\n", integer.c);
+	printf("short %x\n", integer.s);
+	printf("int %x\n", integer.i);
+	printf("long %x\n\n", integer.b);
 	
 	/*LONG SECTION*/
 	printf("Enter data for type long (enter 0 to exit):");
-	scanf("%ld",&userEntry.b);
+	scanf("%ld",&integer.b);
 	fflush(stdin);
 	
-	if(userEntry.s == (long)0)
+	if(integer.s == (long)0)
 	{
 		break;
 	}
 	
 	printf("\nBreakdown of the element in the union\n");
-	printf("char %c\n", userEntry.c);
-	printf("short %hd\n", userEntry.s);
-	printf("int %d\n", userEntry.i);
-	printf("long %ld\n\n", userEntry.b);
+	printf("char %c\n", integer.c);
+	printf("short %hd\n", integer.s);
+	printf("int %d\n", integer.i);
+	printf("long %ld\n\n", integer.b);
 	
 	printf("Breakdown in hex");
-	printf("char %x\n", userEntry.c);
-	printf("short %x\n", userEntry.s);
-	printf("int %x\n", userEntry.i);
-	printf("long %x\n\n", userEntry.b);
+	printf("char %x\n", integer.c);
+	printf("short %x\n", integer.s);
+	printf("int %x\n", integer.i);
+	printf("long %x\n\n", integer.b);
 	
 	}
 	
