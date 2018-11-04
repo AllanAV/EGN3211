6408fec5350dd7f35a0130605c651e288173f734
Allan V Aquino Vieira <aquinovieira@gmail.com>
2018-11-04 18:39:16 -0500

    Add files via upload

--- EGN3211_HW6A.c
+++ EGN3211_HW6A.c
@@ -4,115 +4,112 @@ FALL 2018
 Prof. Sundar Sinnappan
 *** Homework 6A ***
 by Allan Aquino Vieira
-https://github.com/AllanAV/EGN3211/blob/master/EGN3211_HW6A.c
 */
-
-#include <stdio.h>
+ #include <stdio.h>
 #include <stdlib.h>
-
-//Union Definition
-union integer
+ //Union Definition
+union data
 	{
 		char c;
 		short s;
 		int i;
 		long b;
 	};
-
-int main (void)
+ int main (void)
 {
 	//Main Local Variable
-	union integer integer;
+	union data userEntry;
+	userEntry.c = '8';
 	
-	while(integer.c != '0')
+	while(userEntry.c != '0')
 	{
 	/*CHAR SECTION*/
 	printf("Enter data for type char (enter 0 to exit):");
-	scanf("%c", &integer.c);
+	scanf("%c", &userEntry.c);
 	fflush(stdin);
 	
-	if(integer.c == '0')
+	if(userEntry.c == '0')
 	{
 		break;
 	}
 	
 	printf("\nBreakdown of the element in the union\n");
-	printf("char %c\n", integer.c);
-	printf("short %hd\n", integer.s);
-	printf("int %d\n", integer.i);
-	printf("long %ld\n\n", integer.b);
+	printf("char %c\n", userEntry.c);
+	printf("short %hd\n", userEntry.s);
+	printf("int %d\n", userEntry.i);
+	printf("long %ld\n\n", userEntry.b);
 	
 	printf("Breakdown in hex\n");
-	printf("char %x\n", integer.c);
-	printf("short %x\n", integer.s);
-	printf("int %x\n", integer.i);
-	printf("long %x\n\n", integer.b);
+	printf("char %x\n", userEntry.c);
+	printf("short %x\n", userEntry.s);
+	printf("int %x\n", userEntry.i);
+	printf("long %x\n\n", userEntry.b);
 	
 	/*SHORT SECTION*/
 	printf("Enter data for type short (enter 0 to exit):");
-	scanf("%hd",&integer.s);
+	scanf("%hd",&userEntry.s);
 	fflush(stdin);
 	
-	if(integer.s == (short)0)
+	if(userEntry.s == (short)0)
 	{
 		break;
 	}
 	
 	printf("\nBreakdown of the element in the union\n");
-	printf("char %c\n", integer.c);
-	printf("short %hd\n", integer.s);
-	printf("int %d\n", integer.i);
-	printf("long %ld\n\n", integer.b);
+	printf("char %c\n", userEntry.c);
+	printf("short %hd\n", userEntry.s);
+	printf("int %d\n", userEntry.i);
+	printf("long %ld\n\n", userEntry.b);
 	
 	printf("Breakdown in hex");
-	printf("char %x\n", integer.c);
-	printf("short %x\n", integer.s);
-	printf("int %x\n", integer.i);
-	printf("long %x\n\n", integer.b);
+	printf("char %x\n", userEntry.c);
+	printf("short %x\n", userEntry.s);
+	printf("int %x\n", userEntry.i);
+	printf("long %x\n\n", userEntry.b);
 	
 	/*INT SECTION*/
 	printf("Enter data for type int (enter 0 to exit):");
-	scanf("%d",&integer.i);
+	scanf("%d",&userEntry.i);
 	fflush(stdin);
 	
-	if(integer.s == (int)0)
+	if(userEntry.s == (int)0)
 	{
 		break;
 	}
 	
 	printf("\nBreakdown of the element in the union\n");
-	printf("char %c\n", integer.c);
-	printf("short %hd\n", integer.s);
-	printf("int %d\n", integer.i);
-	printf("long %ld\n\n", integer.b);
+	printf("char %c\n", userEntry.c);
+	printf("short %hd\n", userEntry.s);
+	printf("int %d\n", userEntry.i);
+	printf("long %ld\n\n", userEntry.b);
 	
 	printf("Breakdown in hex");
-	printf("char %x\n", integer.c);
-	printf("short %x\n", integer.s);
-	printf("int %x\n", integer.i);
-	printf("long %x\n\n", integer.b);
+	printf("char %x\n", userEntry.c);
+	printf("short %x\n", userEntry.s);
+	printf("int %x\n", userEntry.i);
+	printf("long %x\n\n", userEntry.b);
 	
 	/*LONG SECTION*/
 	printf("Enter data for type long (enter 0 to exit):");
-	scanf("%ld",&integer.b);
+	scanf("%ld",&userEntry.b);
 	fflush(stdin);
 	
-	if(integer.s == (long)0)
+	if(userEntry.s == (long)0)
 	{
 		break;
 	}
 	
 	printf("\nBreakdown of the element in the union\n");
-	printf("char %c\n", integer.c);
-	printf("short %hd\n", integer.s);
-	printf("int %d\n", integer.i);
-	printf("long %ld\n\n", integer.b);
+	printf("char %c\n", userEntry.c);
+	printf("short %hd\n", userEntry.s);
+	printf("int %d\n", userEntry.i);
+	printf("long %ld\n\n", userEntry.b);
 	
 	printf("Breakdown in hex");
-	printf("char %x\n", integer.c);
-	printf("short %x\n", integer.s);
-	printf("int %x\n", integer.i);
-	printf("long %x\n\n", integer.b);
+	printf("char %x\n", userEntry.c);
+	printf("short %x\n", userEntry.s);
+	printf("int %x\n", userEntry.i);
+	printf("long %x\n\n", userEntry.b);
 	
 	}
 	
