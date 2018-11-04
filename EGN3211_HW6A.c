43a4d566270bcd3f24e9282c388346a70510bd01
Allan V Aquino Vieira <aquinovieira@gmail.com>
2018-11-04 17:33:02 -0500

    Add files via upload

---
+++ EGN3211_HW6A.c
@@ -0,0 +1,120 @@
+/*
+UCF EGN 3211
+FALL 2018
+Prof. Sundar Sinnappan
+*** Homework 6A ***
+by Allan Aquino Vieira
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+//Union Definition
+union data
+	{
+		char c;
+		short s;
+		int i;
+		long b;
+	};
+
+int main (void)
+{
+	//Main Local Variable
+	union data userEntry;
+	userEntry.c = '8';
+	
+	while(userEntry.c != '0')
+	{
+	/*CHAR SECTION*/
+	printf("Enter data for type char (enter 0 to exit):");
+	scanf("%c", &userEntry.c);
+	fflush(stdin);
+	
+	if(userEntry.c == '0')
+	{
+		break;
+	}
+	
+	printf("\nBreakdown of the element in the union\n");
+	printf("char %c\n", userEntry.c);
+	printf("short %hd\n", userEntry.s);
+	printf("int %d\n", userEntry.i);
+	printf("long %ld\n\n", userEntry.b);
+	
+	printf("Breakdown in hex\n");
+	printf("char %x\n", userEntry.c);
+	printf("short %x\n", userEntry.s);
+	printf("int %x\n", userEntry.i);
+	printf("long %x\n\n", userEntry.b);
+	
+	/*SHORT SECTION*/
+	printf("Enter data for type short (enter 0 to exit):");
+	scanf("%hd",&userEntry.s);
+	fflush(stdin);
+	
+	if(userEntry.s == (short)0)
+	{
+		break;
+	}
+	
+	printf("\nBreakdown of the element in the union\n");
+	printf("char %c\n", userEntry.c);
+	printf("short %hd\n", userEntry.s);
+	printf("int %d\n", userEntry.i);
+	printf("long %ld\n\n", userEntry.b);
+	
+	printf("Breakdown in hex");
+	printf("char %x\n", userEntry.c);
+	printf("short %x\n", userEntry.s);
+	printf("int %x\n", userEntry.i);
+	printf("long %x\n\n", userEntry.b);
+	
+	/*INT SECTION*/
+	printf("Enter data for type int (enter 0 to exit):");
+	scanf("%d",&userEntry.i);
+	fflush(stdin);
+	
+	if(userEntry.s == (int)0)
+	{
+		break;
+	}
+	
+	printf("\nBreakdown of the element in the union\n");
+	printf("char %c\n", userEntry.c);
+	printf("short %hd\n", userEntry.s);
+	printf("int %d\n", userEntry.i);
+	printf("long %ld\n\n", userEntry.b);
+	
+	printf("Breakdown in hex");
+	printf("char %x\n", userEntry.c);
+	printf("short %x\n", userEntry.s);
+	printf("int %x\n", userEntry.i);
+	printf("long %x\n\n", userEntry.b);
+	
+	/*LONG SECTION*/
+	printf("Enter data for type long (enter 0 to exit):");
+	scanf("%ld",&userEntry.b);
+	fflush(stdin);
+	
+	if(userEntry.s == (long)0)
+	{
+		break;
+	}
+	
+	printf("\nBreakdown of the element in the union\n");
+	printf("char %c\n", userEntry.c);
+	printf("short %hd\n", userEntry.s);
+	printf("int %d\n", userEntry.i);
+	printf("long %ld\n\n", userEntry.b);
+	
+	printf("Breakdown in hex");
+	printf("char %x\n", userEntry.c);
+	printf("short %x\n", userEntry.s);
+	printf("int %x\n", userEntry.i);
+	printf("long %x\n\n", userEntry.b);
+	
+	}
+	
+	return 0;
+}
